d74be5bc8206e17c273921323860d8fc310aae88
evarghese563 <evarghese454@gmail.com>
2024-03-02 19:55:57 -0500

    Added REST API Connection

---
+++ bookstore/books/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.core import serializers
+import requests
+
+
+
+def home(request):
+    url = 'https://openlibrary.org/search.json?q=the+lord+of+the+rings'
+    response = requests.get(url)
+    data = response.json()
+    print(data)
+    return render(request,"books.html",{'data':data})
