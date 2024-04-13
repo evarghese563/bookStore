f78a9b91bfcc457a71f07a47aa8d3a91acc2f115
evarghese563 <evarghese454@gmail.com>
2024-04-13 19:42:18 -0400

    Committing first js function and added values to the items

---
+++ bookstore/books/static/app.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function() {
+    // alert("jQuery is working!");
+
+
+    $('.addToCart').click(function() {
+
+        var productName = $(this).data('loc-name');
+        var productPrice = $(this).data('loc-price');
+
+
+        console.log("Adding to cart:");
+        console.log("Product Name: " + productName);
+        console.log("Product Price: $" + productPrice);
+
+    });
+
+});
+
