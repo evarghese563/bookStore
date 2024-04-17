6acb3cdc51b19a24feafa2b935e56cc5be8d7581
evarghese563 <evarghese454@gmail.com>
2024-04-16 21:17:09 -0400

    VinlandSaga, DB, FMAB, Naruto post into the db, whereas the pages with multiple items on the page don't

--- bookstore/books/static/app.js
+++ bookstore/books/static/app.js
@@ -12,22 +12,9 @@ $(document).ready(function() {
         console.log("Adding to cart:");
         console.log("Product Name: " + productName);
         console.log("Product Price: $" + productPrice);
-
-
-        var items = {
-            Name: productName,
-            Price: productPrice
-        };
-
+        
         alert("You added "+productName+" to the cart")
     });
 
 });
-
-function toCartDB(items){
-    const FileSystem = require("fs");
-    FileSystem.writeFile('cart.json', JSON.stringify(items), (error) => {
-       if (error) throw error;
-     });
-};
 
--- bookstore/books/views.py
+++ bookstore/books/views.py
@@ -19,6 +19,70 @@ def base(request):
 
 def fullmetal(request):
 
+    pushToDB(request)
+
+    return render(request, "fullmetal.html",{})
+
+def vinlandSaga(request):
+    pushToDB(request) 
+    return render(request, "vinlandSaga.html")
+
+def dragonball(request): 
+    pushToDB(request)
+    return render(request, "dragonball.html")
+
+def naruto(request): 
+    pushToDB(request)
+    return render(request, "naruto.html")
+
+def onepiece(request):
+    pushToDB(request) 
+    return render(request, "onepiece.html")
+
+def chainsawman(request):
+    pushToDB(request) 
+    return render(request, "chainsawman.html")
+
+def demonslayer(request):
+    pushToDB(request) 
+    return render(request, "demonslayer.html")
+
+def myheroacademia(request):
+    pushToDB(request) 
+    return render(request, "myheroacademia.html")
+
+def attackontitan(request):
+    
+    pushToDB(request) 
+    return render(request, "attackontitan.html",{})
+
+def tokyoghoul(request):
+    pushToDB(request) 
+    return render(request, "tokyoghoul.html")
+
+def manga(request):
+    return render(request, "manga.html")
+
+def squareenix(request):
+    pushToDB(request) 
+    return render(request, "squareenix.html")
+
+def checkout(request):
+
+    return render(request, "checkout.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def merch(request): 
+    return render(request, "merch.html")
+
+
+
+def pushToDB(request):
     if request.method =='POST':
         name = request.POST['manga']
         price = request.POST['price']
@@ -27,53 +91,3 @@ def fullmetal(request):
 
         newcart = Manga(name = name, price = price, quantity = quantity)
         newcart.save()
-
-    return render(request, "fullmetal.html",{})
-
-def vinlandSaga(request): 
-    return render(request, "vinlandSaga.html")
-
-def dragonball(request): 
-    return render(request, "dragonball.html")
-
-def naruto(request): 
-    return render(request, "naruto.html")
-
-def onepiece(request): 
-    return render(request, "onepiece.html")
-
-def chainsawman(request): 
-    return render(request, "chainsawman.html")
-
-def demonslayer(request): 
-    return render(request, "demonslayer.html")
-
-def myheroacademia(request): 
-    return render(request, "myheroacademia.html")
-
-def attackontitan(request): 
-    return render(request, "attackontitan.html")
-
-def tokyoghoul(request): 
-    return render(request, "tokyoghoul.html")
-
-def manga(request): 
-    return render(request, "manga.html")
-
-def squareenix(request): 
-    return render(request, "squareenix.html")
-
-def checkout(request): 
-    return render(request, "checkout.html")
-
-def about(request): 
-    return render(request, "about.html")
-
-def contact(request): 
-    return render(request, "contact.html")
-
-def merch(request): 
-    return render(request, "merch.html")
-
-
-
